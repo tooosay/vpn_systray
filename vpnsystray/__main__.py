0b2e949822626c2b3244b3fab2fab7d1c95e363d
tooosay <122712967+tooosay@users.noreply.github.com>
2023-10-11 20:11:56 +0900

    enable direct call by pkg name vpnsystray

--- vpnsystray/__main__.py
+++ vpnsystray/__main__.py
@@ -1,5 +1,13 @@
-import vpnsystray
-
+import vpnsystray as app
+import os
 
 def main():
-    vpnsystray()
+    dir_name = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + "/icons/"
+    on_icon_path = dir_name + "vpn_on.png"
+    off_icon_path = dir_name + "vpn_off.png"
+    err_icon_path = dir_name + "vpn_err.png"
+
+    app.vpnsystray(on_icon_path, off_icon_path, err_icon_path)
+
+if __name__ == "__main__":
+    main()
